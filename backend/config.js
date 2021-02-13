39e6cfa09bad366b7db5b8048d134e014ae2024d
Sanjeet Pal Singh <hanspal.sanjit28@gmail.com>
2021-02-13 15:25:55 -0500

    Backend config + database services added

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    SQL_PORT: process.env.SQL_PORT,
+    SQL_HOST: process.env.SQL_HOST,
+    SQL_USERNAME: process.env.SQL_USERNAME,
+    SQL_PASSWORD: process.env.SQL_PASSWORD,
+    SQL_DATABASE: process.env.SQL_DATABASE,
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+    APP_ENV: process.env.APP_ENV,
+    BASE_URL_DEV: "http://localhost:1200",
+    // BASE_URL_PROD: ""
+}
