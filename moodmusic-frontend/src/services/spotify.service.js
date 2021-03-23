ac72d5c6ffb53accdd9bb102e0750160b9db0eec
Sanjeet Pal Singh <hanspal.sanjit28@gmail.com>
2021-03-23 05:48:33 -0400

    spotify service added

---
+++ moodmusic-frontend/src/services/spotify.service.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+const configData = require('../config.json')
+
+export const SpotifyService = {
+    getSearchResult
+};
+
+function getSearchResult(query){
+
+    return new Promise((resolve, reject) => {
+
+        //Get the spotify OAuth access Token
+        
+        var auth = sessionStorage.getItem('auth');
+        if(auth != ""){
+            auth = JSON.parse(auth)
+            const data = {
+                headers: {
+                  'Authorization': `Bearer ${auth.accessToken}`
+                }
+            }
+            const url = `${configData.development.SPOTIFY_URL}/search?q=${query}%20music&type=track&market=US&limit=10`
+            axios.get(url, data).then(res => {
+                
+                resolve(res.data)
+            }).catch((err) => {
+                reject(err)
+            })
+        }
+
+        
+    })
+
+}
+
+
+
